f96e53b02d8a48b098e6ac81de231c093364c125
DariaHaidabura <dariagaidabura99@gmail.com>
2021-05-22 12:46:41 +0300

    added hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import {  Categories, SortPopup,PizzaBlock } from '../components';
+import { useSelector } from 'react-redux';
 
-function Home({ items }) {
+function Home() {
+  const { items } = useSelector(({ pizzas }) => {
+    return {
+      items: pizzas.items,      
+    };
+  }); 
     return (
         <div className="container">
           <div className="content__top">
@@ -17,8 +23,7 @@ function Home({ items }) {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-           {
-             items.map(obj => ( 
+           {items && items.map(obj => ( 
              <PizzaBlock key={obj.id}  {...obj}  />
              ))}
            
